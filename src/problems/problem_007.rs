635da36402fe6bf43dbd2f7dd9bca7dbc814975c
Aditya raj <88555779+aditya-raj-panjiyara@users.noreply.github.com>
2025-11-20 18:25:40 +0530

    added problem 7 question

--- src/problems/problem_007.rs
+++ src/problems/problem_007.rs
@@ -1,5 +1,5 @@
 // Problem 07: 10001st prime
-// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13. find the 10001st prime
 fn is_prime(num: i32) -> bool {
     let limit = (num as f64).sqrt() as i32;
     for i in 2..=limit {
