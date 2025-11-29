5a90a703d10a97e70651e2d78a6a66771b2e1a12
Aditya raj <88555779+aditya-raj-panjiyara@users.noreply.github.com>
2025-11-29 21:14:25 +0530

    added problem 17

---
+++ src/problems/problem_017.rs
@@ -0,0 +1,13 @@
+use crate::utils::math::number_to_words;
+// Problem 17: Number Letter Counts
+// If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+pub fn solve() -> i32 {
+    let mut total_letters: i32 = 0;
+    // println!("{}", number_to_words(115));
+    for i in 1..=1000 {
+        let word = number_to_words(i);
+        // Using len() is fine because these are all ASCII characters
+        total_letters += word.len() as i32;
+    }
+    total_letters
+}
